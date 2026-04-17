05ca42d7308b4fd82a4bb678e309a2a0a34c038d
Fernando Davila <a91403@ualg.pt>
2026-04-18 00:11:03 +0100

    refactor: Format code

--- src/base_dados.py
+++ src/base_dados.py
@@ -7,7 +7,8 @@ data = {"passengers": [], "flights": [], "tickets": []}
 
 # Function to read json files
 def load_data():
-    # Try to open the file, if not create it
+    """Try to open the file, if not create it"""
+
     try:
         with open("database.json", "r") as f:
             return json.load(f)
@@ -17,8 +18,10 @@ def load_data():
         print("Database created")
         return data.copy()
 
-#Function to save json 
+
+# Function to save json
 def save_data(data):
-    #Save data to JSON file
-    with open ("database.json","w") as f:
-        json.dump(data,f, indent=2)
+    """Save data to JSON file"""
+
+    with open("database.json", "w") as f:
+        json.dump(data, f, indent=2)
