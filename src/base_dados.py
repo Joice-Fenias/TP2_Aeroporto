78a44a4b8904bd8fa711499e606cb8a00612a0c7
Joice Fenias <joicefenias@gmail.com>
2026-04-26 15:57:15 +0100

    feat: ajuste de docstings para docs

--- src/base_dados.py
+++ src/base_dados.py
@@ -1,7 +1,14 @@
-# Funções para ler/escrever em ficheiro (JSON ou CSV)
+"""Módulo de acesso à base de dados.
+Contém funções para carregar e guardar dados usando um ficheiro JSON.
+A estrutura de dados é um dicionário com chaves "passageiros", "voos" e "bilhetes", cada uma contendo uma lista de objetos correspondentes.
+As funções de carregamento e guarda são projetadas para serem simples e robustas, lidando com erros comuns como ficheiros ausentes ou dados corrompidos.
+
+..warning::
+Este módulo é responsável apenas pelo acesso à base de dados. Evite colocar lógica de negócio aqui. Use as funções definidas em consola.py para manter a organização."""
+
 import json
 
-arquivo_db="database.json"
+arquivo_db = "database.json"
 
 # Estrutura base para armazenar os dados
 dados = {"passageiros": [], "voos": [], "bilhetes": []}
@@ -9,9 +16,7 @@ dados = {"passageiros": [], "voos": [], "bilhetes": []}
 
 def carregar_dados():
     """Carrega os dados do ficheiro JSON.
-    
-    Se o ficheiro não existir, cria uma estrutura inicial
-    e devolve uma cópia dos dados base.
+    Se o ficheiro não existir, cria um novo com a estrutura base.
     """
 
     try:
@@ -26,9 +31,10 @@ def carregar_dados():
 
 # Função para guardar dados no ficheiro JSON
 def guardar_dados(dados):
-    """Guarda os dados atualizados no ficheiro JSON."""
+    """Guarda os dados atualizados no ficheiro JSON.
+    :param dados: Dicionário contendo as listas de passageiros, voos e bilhetes.
+    :return: None
+    """
 
     with open("database.json", "w") as ficheiro:
         json.dump(dados, ficheiro, indent=2)
-
-
